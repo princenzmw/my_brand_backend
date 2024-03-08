d9770fb44cb5b901513eac2c4b997b89da45f6b1
princenzmw <princenzmw@gmail.com>
2024-03-08 10:14:50 +0200

    The server file

---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import route from './routes/userRoute.js';
+import blgroute from './routes/blogRoute.js';
+
+const app = express();
+app.use(cors());
+
+dotenv.config(); // Load environment variables from .env file
+
+// ... app setup ...
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(express.json()); // For parsing application/json
+
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(() => {
+    console.log('Database Connected to MongoDB successfully');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}).catch(err => console.log(err));
+
+app.use("/api/user", route)
+app.use("/api/blog", blgroute)
+app.use('/api/Media/blogs', express.static('Media/blogs'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
